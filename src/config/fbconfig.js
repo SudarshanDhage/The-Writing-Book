c20505ffbc96e188aed6e5a8513f684bf8987d81
Sudarshan Dhage <86158515+SudarshanDhage@users.noreply.github.com>
2023-04-26 16:31:22 +0530

    Update fbconfig.js

--- src/config/fbconfig.js
+++ src/config/fbconfig.js
@@ -3,12 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: "AIzaSyBKGAt0WDa35X6eXw9NHnjZYmqud4bCQTI",
+    authDomain: "gifted-relic-375009.firebaseapp.com",
+     projectId: "gifted-relic-375009",
+    storageBucket: "gifted-relic-375009.appspot.com",
+     messagingSenderId: "450681843974",
+    appId: "1:450681843974:web:7a59531392a5b7da046e87",
+    measurementId: "G-EJL83V4QLT"
   };
   firebase.initializeApp(config);
   firebase.firestore();
